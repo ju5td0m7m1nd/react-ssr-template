63023a4ef220983d27aecc6cfaf7cd9886807599
Juin Chiu <davidjuin0519@gmail.com>
2016-10-23 22:45:21 +0800

    Implement server render fetching

--- server/server.js
+++ server/server.js
@@ -6,8 +6,9 @@ import { match, RouterContext } from 'react-router'
 import { Provider }from 'react-redux'
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
-import store from '../client/redux/redux-store'
+import configureStore from '../client/redux/redux-store'
 import routes from '../client/routes'
+import { serverRender } from '../actions'
 
 import fs from 'fs'
 import path from 'path'
@@ -34,7 +35,13 @@ app.use((req, res, next) => {
 })
 
 app.get('*', (req, res) => {
+  const muiTheme = getMuiTheme({
+    userAgent: navigator.userAgent
+  })
   const initialState = {}
+  const store = configureStore(initialState)
+  const limit = 10
+  const offset = 20
 
   match({ routes: routes, location: req.url }, function (error, redirectLocation, renderProps) {
     if (error) {
@@ -42,25 +49,28 @@ app.get('*', (req, res) => {
     } else if (redirectLocation) {
       res.redirect(302, redirectLocation.pathname + redirectLocation.search)
     } else if (renderProps) {
-      const muiTheme = getMuiTheme({
-        userAgent: navigator.userAgent,
-      })
+      const comp = renderProps.components[renderProps.components.length - 1].WrappedComponent
+      const func = comp.fetchData
+                   ? comp.fetchData({ store, limit, offset }) 
+                   : Promise.resolve()
 
-      res.send("<!DOCTYPE html>" +
-        ReactDOMServer.renderToString(
-          <MuiThemeProvider muiTheme={ muiTheme }>
-            <Provider store={ store(initialState) }>
-              <RouterContext { ...renderProps } />
-            </Provider>
-          </MuiThemeProvider>
-        )
-      )
+      func.
+        then(() => store.dispatch(serverRender())).
+        then(() => {
+          res.send("<!DOCTYPE html>" +
+            ReactDOMServer.renderToString(
+              <MuiThemeProvider muiTheme={ muiTheme }>
+                <Provider store={ store }>
+                  <RouterContext { ...renderProps } />
+                </Provider>
+              </MuiThemeProvider>
+            )
+          )
+        })
     } else {
       res.status(404).send('Not found')
     }
   })
 })
 
-app.listen(app.get('port'), function () {
-  console.log('Server started: http://localhost:' + app.get('port') + '/')
-})
+app.listen(app.get('port'), () => console.log('Server started: http://localhost:' + app.get('port') + '/'))
